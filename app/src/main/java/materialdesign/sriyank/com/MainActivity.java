11dba9138eafbfca2550befaa7d17a0daf7850af
Rikharthu <uberviolence@gmail.com>
2016-10-06 16:38:44 +0300

    finish

--- app/src/main/java/materialdesign/sriyank/com/MainActivity.java
+++ app/src/main/java/materialdesign/sriyank/com/MainActivity.java
@@ -2,13 +2,18 @@ package materialdesign.sriyank.com;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Author : Sriyank Siddhartha
@@ -20,6 +25,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 	private DrawerLayout mDrawerLayout;
 	private TextView mTxvMenuItem;
+	private Button mLoginButton;
+	private EditText mUsernameEt;
+	private EditText mPasswordEt;
+	private TextInputLayout mInputLayoutName, mInputLayoutPassword;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
+
+		mUsernameEt= (EditText) findViewById(R.id.etUsername);
+		mPasswordEt= (EditText) findViewById(R.id.etPassword);
+
+		mInputLayoutName= (TextInputLayout) findViewById(R.id.inputLayoutUsername);
+		mInputLayoutPassword= (TextInputLayout) findViewById(R.id.inputLayoutPassword);
+
 
 		mTxvMenuItem = (TextView) findViewById(R.id.txvMenuItem);
 
@@ -39,16 +55,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 		ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this,
 				mDrawerLayout,
-				toolbar,
+				toolbar, // linked to toolbar -> syncState()
 				R.string.drawer_open,
 				R.string.drawer_close);
 
 		mDrawerLayout.addDrawerListener(drawerToggle);
 
-		// makes the icon and title in the action bar clickable so that “up” navigation can be provided
+		// specifies whether or not the Home button has the arrow used for Up Navigation next to it.
 //		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		// This method just controls whether to show the Activity icon/logo or not.
-		// Generally shows just the title with caret if passed
+		// specifies whether or not the Home button is shown.
 //		getSupportActionBar().setDisplayShowHomeEnabled(true);
 		/*
 		Both setDisplayHomeAsUpEnabled() and setHomeButtonEnabled() calls onOptionsItemSelected()
@@ -60,9 +75,39 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 		drawerToggle.syncState();
 	}
 
+	public void validateAndLogin(View view){
+		if(validateUsername() && validatePassword()){
+			Toast.makeText(this, "Successfully logged in!", Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	private boolean validatePassword() {
+		String pwd = mPasswordEt.getText().toString().trim();
+		if(pwd.length()<8){
+//			mInputLayoutPassword.setError("Minimum 8 characters required");
+			mPasswordEt.setError("Minimum 8 characters required");
+			return false;
+		}else{
+//			mInputLayoutPassword.setErrorEnabled(false);
+			return true;
+		}
+	}
+
+	private boolean validateUsername() {
+		if(mUsernameEt.getText().toString().isEmpty()){
+			mInputLayoutName.setError("Username cannot be blank!");
+			return false;
+		}else{
+			// remove validation message if it exists
+			mInputLayoutName.setErrorEnabled(false);
+			return true;
+		}
+	}
+
 	@Override // Called when Any Navigation Item is Clicked
 	public boolean onNavigationItemSelected(MenuItem menuItem) {
 		// is VERY similar to onOptionsItemSelected
+
 //		menuItem.setCheckable(true);
 //		menuItem.setChecked(true);  // This helps to know which Menu Item is Clicked
 
@@ -76,11 +121,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 			case R.id.item_android:
 				// Your Item specific Codes
+				Toast.makeText(this, "Best OS evur!", Toast.LENGTH_SHORT).show();
 				break;
 
 			case R.id.item_ios:
 				// Your item specific Codes
 				break;
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(item.getItemId()==android.R.id.home){
+			Toast.makeText(this, "Home button selected!", Toast.LENGTH_SHORT).show();
 		}
 
 		return true;
